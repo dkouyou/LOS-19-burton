80c9a17263bdcc82b3a35d7d8f786a77c4d69360
dkouyou <kouyou.cpr@gmail.com>
2022-05-26 21:54:23 -0400

    Added lineage make file properly

---
+++ lineage_burton.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+#TODO : add those
+# Inherit from instantnoodle device
+#$(call inherit-product, device/motorola/burton/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_burton
+PRODUCT_DEVICE := burton
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Motorola Edge Plus
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Build info
+#BUILD_FINGERPRINT := "motorola/racer_retail/racer:11/RPDS31.Q4U-39-26-14-3/ee443b:user/release-keys"
+BUILD_FINGERPRINT := "motorola/burton_retail/burton:11/S1PB32.41-10-19/a055d:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=burton_retail \
+    PRIVATE_BUILD_DESC="burton_retail-user 11 S1PB32.41-10-19 a055d release-keys"
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
